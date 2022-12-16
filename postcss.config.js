125c43df15fb4d6e57d79f27b96a9ebd4133bd3f
BaiWenJi <baiwenji@kedacom.com>
2022-12-16 18:17:57 +0800

    增加 postcss-pxtorem 插件

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,11 @@
+const pxtorem = require('postcss-pxtorem');
+const pxtoremOptions = {
+	rootValue: 20,
+	replace: true,
+	propList: ['*'],
+	selectorBlackList: ['html'],
+};
+
 module.exports = {
-  plugins: [require('autoprefixer')]
+	plugins: [require('autoprefixer'), pxtorem(pxtoremOptions)],
 };
